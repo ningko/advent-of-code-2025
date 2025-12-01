2214120ef16987ae7e242a297943c221851a6b7d
Martina Garcia <mgarcia.06@outlook.es>
2025-12-01 20:25:02 -0300

    add day 1 part 2 solution

--- day 1/day_1.py
+++ day 1/day_1.py
@@ -3,7 +3,9 @@ with open('./input.txt', 'r') as file:
 
 dial = 50
 zeros = 0
+clicks = 0
 
+# part 1
 for element in arr:
     turn = element[0]
     value = int(element[1:])
@@ -18,3 +20,22 @@ for element in arr:
             zeros = zeros + 1
 
 print (zeros)
+
+# part 2
+dial = 50
+for element in arr:
+    turn = element[0]
+    value = int(element[1:])
+
+    if turn == "R":
+        for _ in range(value):
+            dial = (dial + 1) % 100
+            if dial == 0:
+                clicks = clicks + 1
+    elif turn == "L":
+        for _ in range(value):
+            dial = (dial - 1) % 100
+            if dial == 0:
+                clicks = clicks + 1
+
+print (clicks)
