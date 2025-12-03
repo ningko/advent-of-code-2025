aec90faaaf9ef7f8dd50424e116ab83e9d4c9631
Martina Garcia <mgarcia.06@outlook.es>
2025-12-03 02:36:13 -0300

    add day 2 part 2 solution

--- day 2/day_2.py
+++ day 2/day_2.py
@@ -1,3 +1,5 @@
+import re
+
 with open('./input.txt', 'r') as file:
     arr = file.read().split(',')
     
@@ -8,13 +10,13 @@ for i in ran:
     int_range.append([int(j) for j in i])
 
 invalid_id = 0
+invalid_id_p2 = 0
 
 # part 1
 for element in int_range:
     val_1 = element[0]
     val_2 = element[1]
-    for _ in range(val_1, val_2 + 1):
-        id = _
+    for id in range(val_1, val_2 + 1):
         string_id = str(id)
         first_half = string_id[:len(string_id)//2]
         second_half = string_id[len(string_id)//2:]
@@ -24,3 +26,14 @@ for element in int_range:
 
 print(invalid_id)
 
+# part 2
+for element in int_range:
+    val_1 = element[0]
+    val_2 = element[1] 
+    for id in range(val_1, val_2 + 1):
+        string_id = str(id)
+
+        if re.match(r'^(\d+)\1+$', string_id):
+            invalid_id_p2 += id
+
+print(invalid_id_p2)
