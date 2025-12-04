0678aec8d56d32255f284fa423db2a4c9df1bce2
Martina Garcia <ningarcia.06@gmail.com>
2025-12-04 12:36:03 -0300

    add day 4 part 2 solution + fix typos

--- day 4/day_4.py
+++ day 4/day_4.py
@@ -6,22 +6,48 @@ rows = len(matrix)
 cols = len(matrix[0])
 adjacent_positions = [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]
 accessible_rolls = 0
+removed_total = 0
 
-for element in arr:
-    matrix.append(list(element))
-
+# part 1
 for i in range(rows):
     for j in range(cols):
         if matrix[i][j] == '@':
-            adyacent_rolls = 0
+            adjacent_rolls = 0
             for x, y in adjacent_positions:
                 r = i + x
                 c = j + y
                 if 0 <= r < rows and 0 <= c < cols:
                     if matrix[r][c] == '@':
-                        adyacent_rolls += 1
+                        adjacent_rolls += 1
             
-            if adyacent_rolls <= 3:
+            if adjacent_rolls <= 3:
                 accessible_rolls += 1
 
 print(accessible_rolls)
+
+# part 2
+while True:
+    removable = []
+    
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == '@':
+                adjacent_rolls = 0
+                for x, y in adjacent_positions:
+                    r = i + x
+                    c = j + y
+                    if 0 <= r < rows and 0 <= c < cols:
+                        if matrix[r][c] == '@':
+                            adjacent_rolls += 1
+                
+                if adjacent_rolls <= 3:
+                    removable.append((i, j))
+    if not removable:
+        break
+
+    for i, j in removable:
+        matrix[i][j] = '.'
+
+    removed_total += len(removable)
+
+print(removed_total)
